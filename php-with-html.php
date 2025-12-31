653f9c1527fee5eaf1a32881f5f4eb0aa7a6678b
Sumit Mishra <139990446+Sumitnyu8@users.noreply.github.com>
2025-12-31 14:36:42 +0530

    php with html

--- php-with-html.php
+++ php-with-html.php
@@ -1,5 +1,9 @@
 <?php
 $name="Sumit";
-echo "My name is $name";
-echo "<br>";
+$h2_color="color:orange";
+echo "<h1 style='color:red'> My name is $name </h1>";
+// echo "<br>";
+echo "<h2 style=$h2_color> My name is $name </h2>";
+echo "<h2 style=$h2_color> My name is $name </h2>";
+echo "<h2 style=$h2_color> My name is $name </h2>";
 ?>
